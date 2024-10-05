abaa20b1c47ffa5c29ec1f7bcf83c258732c14e7
Glenix745 <ocmayonglen83@gmail.com>
2024-10-05 16:47:22 +0800

    Added data logger

--- applet-1.js
+++ applet-1.js
@@ -1,11 +1,55 @@
-//Event Function 1
-function updateDisplay() {
-    var textbox = document.getElementById('textbox');
-    var displayText = document.getElementById('displayText');
-    displayText.textContent = textbox.value;
-}
+class dataLogger{
 
-function showAlert() {
-    var textbox = document.getElementById('textbox');
-    alert("Hello " + textbox.value);
+    constructor(buttonId, cardContainerId, clearButtonId, logCountId){
+        this.logButton = document.getElementById(buttonId);
+        this.clearButton = document.getElementById(clearButtonId);
+        this.idContainer = document.getElementById(cardContainerId);
+        this.logCountElement = document.getElementById(logCountId);
+        this.loggedData = [];
+
+        this.logButton.addEventListener('click', () => this.logData());
+        this.clearButton.addEventListener('click', () => this.clearLogs());
+
+    }
+
+    logData(){
+        const time = new Date().toLocaleString();
+        this.loggedData.push(time);
+        this.updateCardContainer();
+    }
+
+    clearLogs(){
+        this.loggedData = []; 
+        this.updateCardContainer();
+       
+    }
+
+    updateCardContainer(){
+        this.idContainer.innerHTML = '';
+    
+        this.loggedData.forEach(data => {       
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.idContainer.appendChild(card);
+        });
+        this.displayLogCount();
+    }
+
+    countLogs(){
+        return this.loggedData.length;
+      }
+
+    displayLogCount() {
+        const logCount = this.countLogs();
+        this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`; 
+    }
 }
+    document.addEventListener('DOMContentLoaded', () => {
+    new dataLogger('logButton', 'idContainer', 'clearButton', 'logCount'); 
+    })
