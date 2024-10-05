635d231591b096926e92cdd9229f8cea855e5e78
Glenix745 <ocmayonglen83@gmail.com>
2024-10-05 15:10:39 +0800

    Added event function 2

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-//Data types
+//Event Function 1
+function updateDisplay() {
+    var textbox = document.getElementById('textbox');
+    var displayText = document.getElementById('displayText');
+    displayText.textContent = textbox.value;
+}
 
-//Arithmetic and logical Operators
-
-//...instructions
+//Event Function 2
+function showAlert() {
+    var textbox = document.getElementById('textbox');
+    var text = textbox.value;
+    alert('You entered: ' + text.value);
+}
