249a5de8aad02c1f1ac9f80d180156ba14787b86
Glenix745 <ocmayonglen83@gmail.com>
2024-10-11 16:06:31 +0800

    Added class cardSearch and constructor

---
+++ index.js
@@ -0,0 +1,11 @@
+class CardSearch {
+    constructor(searchInputId, cardContainerClass) {
+        this.searchInput = document.getElementById(searchInputId);
+        this.cardContainer = document.querySelector(`.${cardContainerClass}`);
+        this.cards = this.cardContainer.querySelectorAll('.card');
+
+        this.init();
+    }
+
+    
+    
